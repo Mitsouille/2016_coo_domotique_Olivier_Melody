a45e406264a0737fde3339da41597e5a7ab79356
Drouvot Melody <melody.drouvot@gmail.com>
2024-04-04 09:23:01 +0200

    Ajout tout les fichiers

---
+++ src/Telecommande.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Telecommande {
+    private ArrayList<Lampe> lampes;
+
+    public Telecommande(){
+        this.lampes = new ArrayList<Lampe>();
+    }
+}
